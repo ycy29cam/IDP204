11351e8d581c297be3405738080948f5299d0fe8
Olly Parker <op301@cam.ac.uk>
2024-02-20 22:36:32 +0000

    drive_route function started and header file included

---
+++ navigation_functions/routes.h
@@ -0,0 +1,25 @@
+// this is a header file to contain the routes in the form 0, 1, 2 for the 21 routes listed in navigation_function
+// can potenitally move to storing some more global variables in here
+// i will fill in these routes on weds 21st (olly) 
+
+int SA[] = {};
+int AG[] = {};
+int AR[] = {};
+int AB[] = {};
+int GB[] = {};
+int RB[] = {};
+int BG[] = {};
+int BR[] = {};
+int BC[] = {};
+int GC[] = {};
+int RC[] = {};
+int CG[] = {};
+int CR[] = {};
+int CD[] = {};
+int GD[] = {};
+int RD[] = {};
+int DG[] = {};
+int DR[] = {};
+int DS[] = {};
+int GS[] = {};
+int RS[] = {};
