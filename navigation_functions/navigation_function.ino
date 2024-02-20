dfc2f70f7813a69b8f63e58ec89826bee1c426c0
Olly Parker <op301@cam.ac.uk>
2024-02-20 19:54:31 +0000

    syntax errors fixed in nav function

--- navigation_functions/navigation_function.ino
+++ navigation_functions/navigation_function.ino
@@ -1,48 +1,43 @@
 #include <Adafruit_MotorShield.h>
 
+// Assuming SA, AG, AR, etc. are defined elsewhere in your code
+int routes[] = {SA, AG, AR, AB, GB, RB, BG, BR, BC, GC, RC, CG, CR, CD, GD, RD, DG, DR, DS, GS, RS};
 
-int routes[21]
-routes = [SA, AG, AR, AB, GB, RB, BG, BR, BC, GC, RC, CG, CR, CD, GD, RD, DG, DR, DS, GS, RS]
+int route_counter = 0;
 
-int route_counter
-route_counter = 0
-
-
-void loop(){
-    // drive to first checkpoint
+void loop() {
+    // drive to the first checkpoint
     drive_route(routes[0]);
+
     // check for block
-    block_present = block_detect();
+    bool block_present = block_detect();
 
     // run delivery of block if present
-    if block_present{
+    if (block_present) {
+        // detect the block's color
+        bool colour_present = colour_detect();
 
-        // detect the block's colour
-        colour_present = colour_detect();
-
-        // pick up the block and return to track
+        // pick up the block and return to the track
         pick_up_block();
 
-        // adjust route depending on platform required
-        if colour_present == black {
-            route_counter += 1
-        }
-        else if colour_present == red {
-            route_counter += 2
+        // adjust route depending on the platform required
+        if (colour_present == black) {
+            route_counter += 1;
+        } else if (colour_present == red) {
+            route_counter += 2;
         }
 
-        // deliver block to platform and proceed to next location
-        drive_route((routes[route_counter]));
+        // deliver block to the platform and proceed to the next location
+        drive_route(routes[route_counter]);
         drop_off_block();
-        route_counter += 3
-        drive_route((routes[route_counter]));
+        route_counter += 3;
+        drive_route(routes[route_counter]);
     }
 
-    // if no block detected, proceed to next station
+    // if no block detected, proceed to the next station
     else if (!block_present) {
         route_counter += 3;
-        drive_route((routes[route_counter]));
-        route_counter += 2
+        drive_route(routes[route_counter]);
+        route_counter += 2;
     }
-     }
-
+}
